1f9543e9c566789c53b27454fe2711671679a277
aibek99 <aibek.bakirov.kk@gmail.com>
2023-03-24 17:45:36 +0300

    Corrected some bugs

--- src/App.svelte
+++ src/App.svelte
@@ -29,46 +29,48 @@
     </nav>
 </div>
 
-<div class="window" id="aboutme">
 
-    <div class="item">
-        <h1>About me</h1>
-        <p>Welcome to my personal webpage! My name is <span>{name}</span> and this website serves as a platform to showcase my work and interests.  
-            Here you will find information about me, my projects, and how to get in touch. I hope you enjoy exploring my site and learning more about me. Thank you for visiting!</p>
+<div class="main-part">
+    <div class="window" id="aboutme">
+
+        <div class="item">
+            <h1>About me</h1>
+            <p>Welcome to my personal webpage! My name is <span>{name}</span> and this website serves as a platform to showcase my work and interests.  
+                Here you will find information about me, my projects, and how to get in touch. I hope you enjoy exploring my site and learning more about me. Thank you for visiting!</p>
+        </div>
+        <ul class="container"> 
+            <li class="item">
+                <h2>Facts about me</h2>
+                <ul class="aboutme">
+                    <li class="item">3rd year student at Innopolis University</li>
+                    <li class="item">major is Software Development</li>
+                    <li class="item">love to solve algorithmic problems</li>
+                    <li class="item">love to play football</li>
+                    <li class="item">
+                        <a href="https://codeforces.com/profile/myway">
+                            Codeforces
+                        </a>
+                        account
+                    </li>
+                    <li class="item">
+                        <a href="https://leetcode.com/aba99/">
+                            Leetcode
+                        </a> 
+                        account
+                    </li>
+                </ul>
+            </li>
+
+            <li class="item">
+                <h2>Comic Image</h2>
+                <GetComic />
+            </li>
+        </ul>
     </div>
-    <ul class="container"> 
-        <li class="item">
-            <h2>Facts about me</h2>
-            <ul class="aboutme">
-                <li class="item">3rd year student at Innopolis University</li>
-                <li class="item">major is Software Development</li>
-                <li class="item">love to solve algorithmic problems</li>
-                <li class="item">love to play football</li>
-                <li class="item">
-                    <a href="https://codeforces.com/profile/myway">
-                        Codeforces
-                    </a>
-                    account
-                </li>
-                <li class="item">
-                    <a href="https://leetcode.com/aba99/">
-                        Leetcode
-                    </a> 
-                    account
-                </li>
-            </ul>
-        </li>
 
-        <li class="item">
-            <h2>Comic Image</h2>
-            <GetComic />
-        </li>
-    </ul>
+
+    <Projects />
 </div>
 
-
-
-
-<Projects />
 <Contacts />
 
